2c5eb4afc6f21685e7f464c807a830850f6ae5fa
I-Zor <ivona.zoricic@gmail.com>
2021-09-17 09:38:52 +0200

    Initial commit

---
+++ src/main/java/com/example/examinationsuppgiftkubernetes/ExaminationsuppgiftKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.examinationsuppgiftkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationsuppgiftKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminationsuppgiftKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examinationsuppgiftkubernetes/ExaminationsuppgiftKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examinationsuppgiftkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationsuppgiftKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
